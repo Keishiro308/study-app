535a18886b32ca9db3d267b80cfcf67aa12b9772
Keishiro Nishiyama <kn11308@gmail.com>
2020-05-31 00:40:37 +0900

    fix webpacker css

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -78,6 +78,6 @@ class NotesController < ApplicationController
   end
 
   def set_tag_white_list
-    @tags_white_list = %w(a acronym b strong i em li ul ol h1 h2 h3 h4 h5 h6 blockquote br cite sub sup ins p img iframe)
+    @tags_white_list = %w(a acronym b strong i em li ul ol h1 h2 h3 h4 h5 h6 blockquote br cite sub sup ins p img iframe span)
   end
 end
